1f8ac3014b0fd86d2b133effa4d1be18a65283f2
Didier Defrance <d2f.defrance@gmail.com>
2018-12-17 15:15:09 +0100

    blabla

--- src/main/java/com/m2i/tp/dao/TestDao.java
+++ src/main/java/com/m2i/tp/dao/TestDao.java
@@ -9,6 +9,12 @@ public class TestDao {
 		// en atendant de voir JUnit plus tard
 		DaoProduitJdbc daoProduit = new DaoProduitJdbc();
 		System.out.println(daoProduit.rechercherProduits());
+		//NB : le Dao pourra acceéder à la base de donnée Mysql 
+		// que si :
+		//			* mysql-connector....jar est bien configuré dans pom.xml
+		//			* le serveur Mysql est démarré (avec ou sans WAMP) .
+		//			* la base de donnée "baseProd" existe sur localhost
+		//			* la table "produit" existe avec structure et données correctes
 	}
 
 }
