7164c69704c7db65813b8680430499e9329f52e3
Didier Defrance <d2f.defrance@gmail.com>
2018-12-17 14:31:26 +0100

    ProduitServlet debut

---
+++ src/main/java/com/m2i/tp/web/ProduitServlet.java
@@ -0,0 +1,33 @@
+package com.m2i.tp.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ProduitServlet
+ */
+@WebServlet("/ProduitServlet")
+public class ProduitServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+      
+	//<a href="./ProduitServlet">liste des produits (servlet+dao+jsp)</a><br/>
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<html><body>");
+		out.println("<h3>liste des produits</h3>");
+		out.println("...");//futur appel sur DAO.
+		out.println("</body></html>");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
