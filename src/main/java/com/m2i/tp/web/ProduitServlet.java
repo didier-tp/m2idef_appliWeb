89e205a6e01412ad5964f072d0e10c97299ca0c1
Didier Defrance <d2f.defrance@gmail.com>
2018-12-17 14:56:39 +0100

    tableau

--- src/main/java/com/m2i/tp/web/ProduitServlet.java
+++ src/main/java/com/m2i/tp/web/ProduitServlet.java
@@ -2,12 +2,17 @@ package com.m2i.tp.web;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.m2i.tp.dao.DaoProduit;
+import com.m2i.tp.dao.DaoProduitJdbc;
+import com.m2i.tp.entity.Produit;
 
 /**
  * Servlet implementation class ProduitServlet
@@ -22,7 +27,17 @@ public class ProduitServlet extends HttpServlet {
 		PrintWriter out = response.getWriter();
 		out.println("<html><body>");
 		out.println("<h3>liste des produits</h3>");
-		out.println("...");//futur appel sur DAO.
+		DaoProduit daoProduit = new DaoProduitJdbc();
+		List<Produit> listeProduits = daoProduit.rechercherProduits();
+		out.println("<table border='1'>");
+		for(Produit prod : listeProduits) {
+			out.println("<tr>");
+				out.println("<td>"+ prod.getNumero() + "</td>");
+				out.println("<td>"+ prod.getLabel() + "</td>");
+				out.println("<td>"+ prod.getPrix() + "</td>");
+			out.println("</tr>");
+		}
+		out.println("</table>");
 		out.println("</body></html>");
 	}
 
