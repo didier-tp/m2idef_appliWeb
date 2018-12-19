bfccb7b28ca07676dece1933b0ab4d5242783f5e
Didier Defrance <d2f.defrance@gmail.com>
2018-12-19 15:32:44 +0100

    avec ds sans bug

--- src/main/java/com/m2i/tp/dao/MyJdbcUtil.java
+++ src/main/java/com/m2i/tp/dao/MyJdbcUtil.java
@@ -12,21 +12,18 @@ import javax.sql.DataSource;
 import com.mchange.v2.c3p0.ComboPooledDataSource;
 
 public class MyJdbcUtil {
-	
 	private static DataSource ds =null;
 	
 	public static DataSource initDataSource() {
 		 //plein de variantes : avec ou sans pool/recyclage
-		                                   //      via lookup JNDI et serveur JEE ou via techno et paramétrage de l'appli
+		 //      via lookup JNDI et serveur JEE ou via techno et paramétrage de l'appli
 		if(ds==null) {
 			ComboPooledDataSource cpds = new ComboPooledDataSource();
+			try{ cpds.setDriverClass("com.mysql.cj.jdbc.Driver"); }catch(Exception e) { e.printStackTrace();}
 		    cpds.setJdbcUrl("jdbc:mysql://localhost:3306/baseprod?serverTimezone=UTC");
-			cpds.setUser("root");
-			cpds.setPassword("");
-		    // Optional Settings
-		   	cpds.setInitialPoolSize(2);
-		   	cpds.setMinPoolSize(2);
-		   	cpds.setMaxPoolSize(10);
+			cpds.setUser("root");	cpds.setPassword("");
+		    // Optional Settings:
+		   	cpds.setInitialPoolSize(2);		   	cpds.setMinPoolSize(2);		   	cpds.setMaxPoolSize(10);
 		   	System.out.println("cpds="+cpds.toString());
 		    ds=cpds;
 		}
@@ -34,10 +31,10 @@ public class MyJdbcUtil {
 	}
 	
 	public static /* ou pas static*/ Connection seConnecter() {
-		DataSource ds = initDataSource();
+		DataSource datasource = initDataSource();
 		Connection cn = null;
 		try {
-			cn = ds.getConnection(); //appel simple sans aucun paramètre à fournir
+			cn = datasource.getConnection(); //appel simple sans aucun paramètre à fournir
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
